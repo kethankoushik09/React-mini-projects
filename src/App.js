bc8944beba073503d65653286f2e34ede552e505
kethan <kethankoushik09@gmail.com>
2024-12-10 14:24:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import accordion from "./components/Accordion/index";
+// import { Accordion } from "./components/Accordion/index";
+// import RandomColor from "./components/Random-color.js";
+// import StarRating from "./components/Star-Rating.js/index";
+// import { Photoslider } from "./components/photo-slider";
+// import TreeView from "./components/Tree-view";
+// import menus from "./components/Tree-view/data";
+// import Loadmore from "./components/load-more-button";
+// import Lightdark from "./components/light-dark-mode";
+// import Qrcode from "./components/Qr-code";
+// import Scrollindicator from "./components/scroll-indicator.js";
+import Tabslist from "./components/custom-tab/Tab";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="outer">
+        {/* <RandomColor/> */}
+        {/* <StarRating/> */}
+        {/* <Photoslider/> */}
+        {/* <Loadmore/> */}
+        {/* <TreeView menus={menus}/> */}
+        {/* <Qrcode/> */}
+        {/* <Lightdark/> */}
+        {/* <Scrollindicator url={"https://dummyjson.com/products?limit=100"}/> */}
+        <Tabslist/>
+      </div>
+    </>
   );
 }
 
